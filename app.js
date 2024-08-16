88c421382bff9b7eef439703fe48875fd3156326
GorMartirosian <gormartiros2002@gmail.com>
2024-08-16 15:36:09 +0400

    Fixes

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const opLib = require("./element-operations");
 const io = require("./utils/io");
 const { toTitleCase } = require("./utils/str-manipulation");
 const { getRandomNumber } = require("./utils/num-manipulation");
+const { timeout } = require("puppeteer");
 
-(async () => {
+async function main() {
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: false,
@@ -23,7 +24,10 @@ const { getRandomNumber } = require("./utils/num-manipulation");
   //MOCK SAMPLES
   const userLogin = "gor_martirosyan@edu.aua.am";
   const userPassword = "200218";
-  const classesToRegister = ["ESS101", "FND102"];
+  const classesToRegister = [
+    //"CS107C",
+    "CS112D",
+  ];
   const userName = "Gor";
   const userSurname = "Martirosyan";
 
@@ -35,16 +39,24 @@ const { getRandomNumber } = require("./utils/num-manipulation");
     await page.reload();
   }
 
-  //login button
-  await page.locator("a ::-p-text(LOGIN)").hover();
-  await page.locator("div ::-p-text(Student)").click();
-
+  await openLoginPage(page);
   await login(page, userLogin, userPassword, userName, userSurname);
   await openRegistrationPanel(page);
+  await listAllCourses(page);
+  await registerForClasses(page, classesToRegister);
+}
 
-  //loop and register for each one
-  //for each registration, notify the user
-})();
+main();
+
+async function openLoginPage(page) {
+  await page.locator("a ::-p-text(LOGIN)").hover();
+  await page.locator("div ::-p-text(Student)").click();
+}
+
+async function listAllCourses(page) {
+  await page.locator("a ::-p-text(View All)").hover();
+  await page.locator("a ::-p-text(View All)").click();
+}
 
 async function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -53,12 +65,24 @@ async function sleep(ms) {
 async function getClassesToRegister() {
   return (
     await io.getUserInput(
-      "Type the classes you want to register separated by COMAS (,)(Example: CS121 , ESS101 ,..., FND101 ):\n\n"
+      "Type the classes you want to register separated by COMAS (,) (Example: CS121 , ESS101 ,..., FND101 ):\n\n"
     )
   )
     .replace(/\s+/g, "")
     .split(",")
     .map((className) => className.toUpperCase());
+}
+
+function getClassIdentifier(fullClassName) {
+  const regex = /([a-zA-z]){2,4}([0-9]{3})/;
+  const match = regex.exec(fullClassName);
+  return match[0];
+}
+
+function getClassSection(fullClassName) {
+  const regex = /([a-z]|[A-Z])?$/;
+  const match = regex.exec(fullClassName);
+  return match[0];
 }
 
 async function loginPageNotLoaded(page) {
@@ -80,7 +104,7 @@ async function checkIfElementExists(page, selector, timeout = 3000) {
   }
 }
 
-async function isTextVisible(page, uniqueTextInsidePage, timeout = 3000) {
+async function isTextVisible(page, uniqueTextInsidePage, timeout = 0) {
   return await checkIfElementExists(
     page,
     `::-p-xpath(//*[contains(text(), "${uniqueTextInsidePage}")])`,
@@ -119,32 +143,45 @@ async function openRegistrationPanel(page) {
   await page.locator("a ::-p-text(Registration)").click();
 }
 
-//Returns boolean of wether the class was successfully registered or not.
-async function registerForClass(page, className) {
-  while (!(await isTextVisible(page, className, getRandomNumber(3000, 6000)))) {
-    await page.reload();
-  }
-
-  await page.locator(`::-p-text(${className})`).hover();
-  if (await checkIfElementExists(page, `a ::-p-text(${className})`)) {
-    await page.locator(`a ::-p-text(${className})`).click();
-    return true;
-  }
-  if (await checkIfElementExists(page, `button ::-p-text(${className})`)) {
-    await page.locator(`button ::-p-text(${className})`).click();
+async function registerForClass(page, classFullName) {
+  const className = getClassIdentifier(classFullName);
+  const classSection = getClassSection(classFullName);
+  let pathTrailingPart =
+    classSection === ""
+      ? "]"
+      : `and contains(../../td[5]/font/text(), '${classSection}')]`;
+  const xpathExpression =
+    `//a[contains(./font/text(), '${className}')` + pathTrailingPart;
+  const elementHandle = await page
+    .waitForSelector("::-p-xpath(" + xpathExpression + ")", { timeout: 3000 })
+    .catch(() => null);
+  if (elementHandle !== null) {
+    await opLib.click(elementHandle);
+    await completeRegistrationForClass(page);
+    notifyAboutRegistration(classFullName);
     return true;
   }
   return false;
 }
 
+async function completeRegistrationForClass(page) {
+  await page.locator(`a ::-p-text(Complete Registration)`).click();
+  await page.locator(`a ::-p-text(Complete Registration)`).click();
+}
+
+function notifyAboutRegistration(className) {
+  console.log(`Regsitered for the ${className}`);
+}
+
 async function registerForClasses(page, classes) {
   while (classes.length !== 0) {
     for (let i = 0; i < classes.length; i++) {
-      await sleep(getRandomNumber(4000, 8000));
+      await sleep(getRandomNumber(4000, 6000));
       if (await registerForClass(page, classes[i])) {
         classes.splice(i, 1);
       }
     }
+    page.reload();
   }
   console.log("Registered for all the classes!");
 }
